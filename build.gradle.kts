eb49238bc50ac22b48c4b2f1d55ff6b42d2170c8
Vemund Santi <vemund@santi.no>
2020-04-05 14:44:17 +0200

    Create multi module Gradle build

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("org.springframework.boot") version "2.2.6.RELEASE"
+    id("io.spring.dependency-management") version "1.0.9.RELEASE"
+    kotlin("jvm") version "1.3.71"
+    kotlin("plugin.spring") version "1.3.71"
+}
+
+allprojects {
+    repositories {
+        mavenCentral()
+    }
+}
--- coffee-backend/build.gradle.kts
+++ coffee-backend/build.gradle.kts
@@ -1,19 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.2.6.RELEASE"
-	id("io.spring.dependency-management") version "1.0.9.RELEASE"
-	kotlin("jvm") version "1.3.71"
-	kotlin("plugin.spring") version "1.3.71"
+	kotlin("jvm")
+	kotlin("plugin.spring")
 }
-
-group = "com.coffeewithme"
+/*
+group = "chat.letscoffee"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
-
-repositories {
-	mavenCentral()
-}
+*/
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
@@ -21,7 +16,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("com.google.firebase:firebase-admin:6.12.2")
+	implementation("com.google.firebase:firebase-admin")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
