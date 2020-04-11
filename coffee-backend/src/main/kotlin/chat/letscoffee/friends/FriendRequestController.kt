988861185560066361af79b60a3a99478354442a
Vegard Hellem <vhellem@gmail.com>
2020-04-11 14:44:01 +0200

    Refactor: Refactor away APIResponse

--- coffee-backend/src/main/kotlin/chat/letscoffee/friends/FriendRequestController.kt
+++ coffee-backend/src/main/kotlin/chat/letscoffee/friends/FriendRequestController.kt
@@ -1,12 +1,13 @@
 package chat.letscoffee.friends
 
 import chat.letscoffee.exception.ResourceNotFoundException
-import chat.letscoffee.payload.ApiResponse
 import chat.letscoffee.security.model.User
 import chat.letscoffee.security.repository.UserRepository
 import chat.letscoffee.security.security.CurrentUser
 import chat.letscoffee.security.security.UserPrincipal
 import org.springframework.data.repository.findByIdOrNull
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
 
@@ -40,9 +41,9 @@ class FriendRequestController(private val userRepository: UserRepository, privat
 
     @DeleteMapping("delete")
     @PreAuthorize("hasRole('USER')")
-    fun deleteFriendRequest(@CurrentUser userPrincipal: UserPrincipal, @RequestParam("id") id: Long): ApiResponse {
+    fun deleteFriendRequest(@CurrentUser userPrincipal: UserPrincipal, @RequestParam("id") id: Long): ResponseEntity<String> {
         val me: User = userRepository.findByIdOrNull(userPrincipal.id)?: throw ResourceNotFoundException("User", "id", userPrincipal.id!!)
-        return if (service.deleteRequest(me, id))  ApiResponse(true, "Request deleted") else ApiResponse(false, "Friend Request not found or not sent by you");
+        return if (service.deleteRequest(me, id))  ResponseEntity.ok("Request deleted") else ResponseEntity("Friend Request not found or not sent by you", HttpStatus.NOT_FOUND);
     }
 
 }
