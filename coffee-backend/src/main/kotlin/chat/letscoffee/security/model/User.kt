293a5519eb3f7f8a607af38f57dd104667f93596
Vemund Santi <vemund@santi.no>
2020-04-12 16:10:29 +0200

    feat: use JsonIdentityInfo to break friendship recursion

--- coffee-backend/src/main/kotlin/chat/letscoffee/security/model/User.kt
+++ coffee-backend/src/main/kotlin/chat/letscoffee/security/model/User.kt
@@ -1,18 +1,12 @@
 package chat.letscoffee.security.model
 
-import com.fasterxml.jackson.annotation.JsonIgnore
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import lombok.EqualsAndHashCode
-import lombok.ToString
+import com.fasterxml.jackson.annotation.*
 import javax.persistence.*
 import javax.validation.constraints.Email
 
 
-@ToString(exclude = arrayOf("friends"))
-@EqualsAndHashCode(exclude = arrayOf("friends"))
-@NamedEntityGraph(name="graph.Person.friends", // Creating a graph to help hibernate to create a query with outer join.
-        attributeNodes = arrayOf(NamedAttributeNode("friends")))
 @Entity
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator::class, property = "id")
 @Table(name = "users", uniqueConstraints = [UniqueConstraint(columnNames = arrayOf("email"))])
 class User(
         @Id
@@ -33,19 +27,14 @@ class User(
         var provider: AuthProvider,
         @JsonIgnore
         var providerId: String? = null, // Is it used? Do we need it?
+
+        @ManyToMany(cascade = [CascadeType.ALL])
+        val friends: MutableSet<User> = mutableSetOf(),
+
         @JsonIgnore
-        @ManyToMany(cascade = arrayOf(CascadeType.ALL))
-        @JoinTable(name="friends",
-                joinColumns = arrayOf(JoinColumn(name = "friend1_id", referencedColumnName = "id")),
-                inverseJoinColumns = arrayOf(JoinColumn(name = "friend2_id", referencedColumnName = "id")))
-        val friends: MutableSet<User> = mutableSetOf<User>()
-
-
-)
-
-
-
-{
+        @ManyToMany(mappedBy="friends")
+        val friendOf: MutableSet<User> = mutableSetOf()
+) {
         fun addFriend(user: User) {
                 friends.add(user)
         }
