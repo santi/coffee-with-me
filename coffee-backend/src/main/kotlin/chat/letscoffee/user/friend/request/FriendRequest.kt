f7b528bea824a9523d4d2be15e2424a84e569b66
Vemund Santi <vemund@santi.no>
2020-04-13 12:40:39 +0200

    refactor: change index and column names for friendRequests

--- coffee-backend/src/main/kotlin/chat/letscoffee/user/friend/request/FriendRequest.kt
+++ coffee-backend/src/main/kotlin/chat/letscoffee/user/friend/request/FriendRequest.kt
@@ -1,19 +1,38 @@
 package chat.letscoffee.user.friend.request
 
 import chat.letscoffee.user.User
+import java.time.Instant
+import java.time.Instant.now
 import javax.persistence.*
 
 
 @Entity
 @Table(name = "friend_request")
 data class FriendRequest(
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
+
     @ManyToOne
+    @JoinColumn(name = "user_id_from", foreignKey = ForeignKey(name = "friend_request_user_id_from_fkey"))
     val from: User,
+
     @ManyToOne
+    @JoinColumn(name = "user_id_to", foreignKey = ForeignKey(name = "friend_request_user_id_to_fkey"))
     val to: User,
+
+    @Column(nullable = false)
+    val created: Instant = now(),
+
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "TEXT", nullable = false)
+    var status: FriendRequestStatus = FriendRequestStatus.PENDING,
+
     @Column
-    var accepted: Boolean? = null
+    var responded: Instant? = null
 )
+
+enum class FriendRequestStatus {
+    PENDING, ACCEPTED, REJECTED
+}
