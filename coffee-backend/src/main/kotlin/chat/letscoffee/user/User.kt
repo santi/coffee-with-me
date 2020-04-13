7cef58191c737bf3ba122d9765094050b9b586c7
Vemund Santi <vemund@santi.no>
2020-04-13 12:58:42 +0200

    refactor: change index names for user_friend table. Add creation date

--- coffee-backend/src/main/kotlin/chat/letscoffee/user/User.kt
+++ coffee-backend/src/main/kotlin/chat/letscoffee/user/User.kt
@@ -1,48 +1,68 @@
 package chat.letscoffee.user
 
 import chat.letscoffee.security.model.AuthProvider
-import com.fasterxml.jackson.annotation.*
+import com.fasterxml.jackson.annotation.JsonIdentityInfo
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.ObjectIdGenerators
+import java.time.Instant
 import javax.persistence.*
 import javax.validation.constraints.Email
 
 
 @Entity
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator::class, property = "id")
-@Table(name = "user_account", uniqueConstraints = [UniqueConstraint(columnNames = arrayOf("email"))])
+@Table(name = "user_account", uniqueConstraints = [UniqueConstraint(columnNames = arrayOf("email"), name = "user_account_email_uix")])
 class User(
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        val id: Long? = null,
-        @Column(nullable = false)
-        var name: String,
-        @Column(nullable = false)
-        var email: @Email String,
-        var imageUrl: String? = null,
-        @Column(nullable = false)
-        @JsonIgnore
-        var emailVerified: Boolean = false,
-        @JsonIgnore
-        var password: String? = null,
-        @Enumerated(EnumType.STRING)
-        @JsonIgnore
-        var provider: AuthProvider,
-        @JsonIgnore
-        var providerId: String? = null, // Is it used? Do we need it?
 
-        @JsonIgnore
-        @ManyToMany(cascade = [CascadeType.ALL])
-        @JoinTable(name = "user_friend",
-            joinColumns = [JoinColumn(name = "user_id_from")],
-            inverseJoinColumns = [JoinColumn(name = "user_id_to")])
-        val friends: MutableSet<User> = mutableSetOf(),
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
 
-        @JsonIgnore
-        @ManyToMany(mappedBy = "friends")
-        val friendOf: MutableSet<User> = mutableSetOf()
+    @Column(columnDefinition = "TEXT", nullable = false)
+    var name: String,
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    var email: @Email String,
+
+    @JsonIgnore
+    @Column(nullable = false)
+    var emailVerified: Boolean = false,
+
+    @JsonIgnore
+    @Column(columnDefinition = "TEXT")
+    var password: String? = null,
+
+    @Column(columnDefinition = "TEXT")
+    var imageUrl: String? = null,
+
+    @JsonIgnore
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "TEXT", nullable = false)
+    var provider: AuthProvider,
+
+    @JsonIgnore
+    @Column(columnDefinition = "TEXT")
+    var providerId: String? = null, // TODO: Is it used? Do we need it?
+
+    @JsonIgnore
+    @ManyToMany(cascade = [CascadeType.ALL])
+    @JoinTable(name = "user_friend",
+        joinColumns = [JoinColumn(name = "user_id_from", foreignKey = ForeignKey(name = "user_friend_user_id_from_fkey"))],
+        inverseJoinColumns = [JoinColumn(name = "user_id_to", foreignKey = ForeignKey(name = "user_friend_user_id_to_fkey"))])
+    val friends: MutableSet<User> = mutableSetOf(),
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "friends")
+    val friendOf: MutableSet<User> = mutableSetOf(),
+
+    @JsonIgnore
+    @Column(nullable = false)
+    val created: Instant = Instant.now()
+
 ) {
-        fun addFriend(user: User) {
-                friends.add(user)
-        }
+    fun addFriend(user: User) {
+        friends.add(user)
+    }
 }
 
 
